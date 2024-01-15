55e953524fce5464c72f43020e7493455bd92783
Roman Syrbu <romik@pop-os.localdomain>
2024-01-16 00:46:07 +0300

    added link verify

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "github.com/sssyrbu/save-links-telegram-bot/config"
     "log"
+    "github.com/sssyrbu/save-links-telegram-bot/config"
+    "github.com/sssyrbu/save-links-telegram-bot/verify"
     tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
 
 func main() {
     botApiToken := config.LoadConfig().Token
@@ -29,15 +31,20 @@ func main() {
         switch update.Message.Command() {
         case "start":
             msg.Text = "hi there! send me an article and I will save it to my local db. I will be randomly sending you an article every day."
-        case "sayhi":
-            msg.Text = "Hi :)"
-        }
         msg.ReplyToMessageID = update.Message.MessageID
+        default:
+            // Checking if url that was sent by user is valid
+            sent_url := verify.VerifyLink(update.Message.Text)
+            if sent_url {
+                // add to db
+                msg.Text = "valid link" 
+            } else {
+                msg.Text = "invalid link" 
+            }
+        }
 
         if _, err := bot.Send(msg); err != nil {
             panic(err)
         }
     }
-
 }
-
---
+++ verify/verify.go
@@ -0,0 +1,19 @@
+package verify
+
+import (
+    "net/http"
+)
+
+func VerifyLink(link string) bool {
+    res, err := http.Get(link)
+    if err != nil {
+        return false
+    }
+
+    if (res.StatusCode != 200) {
+        return false
+    }
+
+    return true
+
+}
